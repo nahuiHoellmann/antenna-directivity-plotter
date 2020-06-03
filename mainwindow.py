06b30eee73e1704bf0273e0c848333cc2c346af5
Nahui <nahui.hoellmann@rwth-aachen.de>
2020-06-03 14:09:32 +0200

    Change input feedback for lock_label

--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QFileDialog, QMessageBox, QMainWindow
 from PyQt5.QtCore import QTimer
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.figure import Figure
@@ -7,7 +7,6 @@ from matplotlib.figure import Figure
 from antools import Plotter
 import pandas as pd
 from functools import wraps
-
 
 def updates_setting(f):
     """
@@ -20,12 +19,11 @@ def updates_setting(f):
     return wrapper
 
 
-class MainWindow:
+class MainWindow(QMainWindow):
     def __init__(self):
-        window = uic.loadUi("mainwindow.ui")
-        self.window = window
+        QMainWindow.__init__(self)
+        uic.loadUi("mainwindow.ui", self)
         self.setup_gui()
-
         self.init_settings()
 
     def setup_gui(self):
@@ -34,11 +32,11 @@ class MainWindow:
         -Add the plotting canvas to the gui
         -Delegate registering signals
         """
-        self.window.setWindowTitle("[ No open file ]")
+        self.setWindowTitle("[ No open file ]")
 
         # Add the matplotlib canvas to the window
         canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        self.window.horizontalLayout.replaceWidget(self.window.placeholder, canvas)
+        self.horizontalLayout.replaceWidget(self.placeholder, canvas)
 
         # The matplotlib Axes to draw to i.e. ax.plot()
         self.ax = canvas.figure.subplots(subplot_kw=dict(polar=True))
@@ -47,32 +45,32 @@ class MainWindow:
 
     def register_signals(self):
 
-        self.window.choose_file_btn.clicked.connect(
+        self.choose_file_btn.clicked.connect(
             # Don't change to self.set_file!
             lambda _: self.set_file()
         )
 
-        self.window.plot_btn.clicked.connect(
+        self.plot_btn.clicked.connect(
             self.plot
         )
 
-        self.window.lock_input.returnPressed.connect(
-            self.parse_degree_input
+        self.lock_input.textEdited.connect(
+            self.validate_input
         )
 
-        self.window.freq_selector.currentTextChanged.connect(
+        self.freq_selector.currentTextChanged.connect(
             self.set_freq
         )
 
-        self.window.plot_theta_btn.toggled.connect(
+        self.plot_theta_btn.toggled.connect(
             lambda selected: self.set_lock_var("Phi" if selected else "Theta")
         )
 
-        self.window.set_e_phi.stateChanged.connect(
+        self.set_e_phi.stateChanged.connect(
             lambda state: self.update_polarization('E-Phi', state)
         )
 
-        self.window.set_e_theta.stateChanged.connect(
+        self.set_e_theta.stateChanged.connect(
             lambda state: self.update_polarization('E-Theta', state)
         )
 
@@ -88,8 +86,10 @@ class MainWindow:
         self.freq = None
         self.filename = None
 
+        self.valid_input = False
+
         # The user needs to specify a file first
-        self.window.plot_controls.setEnabled(False)
+        self.plot_controls.setEnabled(False)
 
     def update_gui(self):
         """
@@ -100,7 +100,7 @@ class MainWindow:
         the plot button is active as long as all plot setting have a valid value
         """
         if self.file_did_finish_loading:
-            self.window.plot_controls.setEnabled(True)
+            self.plot_controls.setEnabled(True)
 
             plot_prequesites = all([
                 self.lock_var,
@@ -110,13 +110,10 @@ class MainWindow:
                 len(self.polarization) > 0
             ])
 
-            self.window.plot_btn.setEnabled(plot_prequesites)
+            self.plot_btn.setEnabled(plot_prequesites)
 
         else:
-            self.window.plot_controls.setEnabled(False)
-
-    def show(self):
-        self.window.show()
+            self.plot_controls.setEnabled(False)
 
     def plot(self):
         """
@@ -141,27 +138,40 @@ class MainWindow:
             self.ax.figure.canvas.draw_idle()
 
     @updates_setting
-    def parse_degree_input(self):
+    def validate_input(self, text):
+    
+        deg = None
+    
+        if text:
+            try:
+                deg = int(text)
+            except ValueError:
+                self.lock_deg = None
+            else:
+                if self.lock_var == "Theta":
+                    deg_max = 180
+                else:
+                    deg_max = 360
 
-        try:
-            deg = int(self.window.lock_input.text())
-        except ValueError:
-            pass
+                if 0 < deg < deg_max:
+                    self.lock_deg = 0
+                else:
+                    self.lock_deg = None
         else:
-            if self.lock_var == "Theta":
-                deg_max = 180
-            else:
-                deg_max = 360
+            self.lock_deg = None
 
-            if 0 > deg:
-                self.lock_deg = 0
-            elif deg > deg_max:
-                self.lock_deg = deg_max
-            else:
-                self.lock_deg = deg
+        inputIsValid = (text == "" or deg is not None)
 
-        new_text = str(self.lock_deg) if self.lock_deg is not None else ""
-        self.window.lock_input.setText(new_text)
+        if inputIsValid and not self.valid_input:
+            self.valid_input = True
+            self.lock_input.setStyleSheet("")
+        elif not inputIsValid and self.valid_input:
+            self.valid_input = False
+            self.lock_input.setStyleSheet(f"QLineEdit {'{'} background: rgb(255,178,174); selection-background-color: rgb(233, 99, 0); {'}'}")
+
+        
+
+        
 
     @updates_setting
     def update_polarization(self, pol_name, state):
@@ -177,16 +187,16 @@ class MainWindow:
     @updates_setting
     def set_lock_var(self, var):
         self.lock_var = var
-        self.window.label_lock.setText(f'Lock {var} at:')
+        self.label_lock.setText(f'Lock {var} at:')
 
     @updates_setting
     def set_file(self):
-        filename = QFileDialog.getOpenFileName(self.window, filter="*.xlsx")[0]
+        filename = QFileDialog.getOpenFileName(self, filter="*.xlsx *.xls")[0]
 
         if filename:
 
             self.filename = filename
-            self.window.setWindowTitle("Loading file...")
+            self.setWindowTitle("Loading file...")
             self.file_did_finish_loading = False
             QTimer.singleShot(20, self.load_file)
 
@@ -194,7 +204,7 @@ class MainWindow:
     def load_file(self):
 
         self.xl = pd.ExcelFile(self.filename)
-        self.window.freq_selector.clear()
-        self.window.freq_selector.addItems(self.xl.sheet_names)
-        self.window.setWindowTitle(self.filename)
+        self.freq_selector.clear()
+        self.freq_selector.addItems(self.xl.sheet_names)
+        self.setWindowTitle(self.filename)
         self.file_did_finish_loading = True
